bc79de9acfdd07c6ce039671213621515cfeff3f
Nicole O'Connor <lavacano@lavacano.net>
2020-04-21 16:53:57 -0700

    move stub updates to base/updates.zsh, allow install from stub

--- base/updates.zsh
+++ base/updates.zsh
@@ -6,4 +6,16 @@ if type git &> /dev/null; then
     git pull &> ~/.local/share/zeesh/pull.log
 
     cd "${start_dir}"
+
+    _zshrc_updates_pending=0
+
+    if type md5sum &> /dev/null; then
+            cmp -s ~/.zshrc ~/.local/share/zeesh/zshrc-stub
+            if [[ $? -eq 1 ]]; then
+                    cp ~/.local/share/zeesh/zshrc-stub ~/.zshrc
+                    _zshrc_updates_pending=1
+            fi
+    fi
+    
+    [[ _zshrc_updates_pending -eq 1 ]] && printf "zeesh: stub updates pending\n"
 fi
