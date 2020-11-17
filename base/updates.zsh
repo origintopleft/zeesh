78f2496c200855545b67e7acb9880b40d7f12b11
Nicole O'Connor <lavacano@lavacano.net>
2020-11-17 15:44:13 -0800

    move update code into function

--- base/updates.zsh
+++ base/updates.zsh
@@ -1,21 +1,25 @@
-if type git &> /dev/null && (( _zeesh_fresh_install != 1 )); then
-    ticker_message "zeesh: pulling from git" # TODO: less frequently, and background
-    start_dir="$(pwd)"
-
-    cd ~/.local/share/zeesh
-    git pull &> ~/.local/share/zeesh/pull.log
-
-    cd "${start_dir}"
-
-    _zshrc_updates_pending=0
-
-    if type md5sum &> /dev/null; then
-            cmp -s ~/.zshrc ~/.local/share/zeesh/zshrc-stub
-            if [[ $? -eq 1 ]]; then
-                    cp ~/.local/share/zeesh/zshrc-stub ~/.zshrc
-                    _zshrc_updates_pending=1
-            fi
-    fi
+function zeesh_update () {
+    if type git &> /dev/null && (( _zeesh_fresh_install != 1 )); then
+        ticker_message "zeesh: pulling from git" # TODO: less frequently, and background
+        start_dir="$(pwd)"
     
-    [[ _zshrc_updates_pending -eq 1 ]] && ticker_message "zeesh: stub updates pending"
-fi
+        cd ~/.local/share/zeesh
+        git pull &> ~/.local/share/zeesh/pull.log
+    
+        cd "${start_dir}"
+    
+        _zshrc_updates_pending=0
+    
+        if type md5sum &> /dev/null; then
+                cmp -s ~/.zshrc ~/.local/share/zeesh/zshrc-stub
+                if [[ $? -eq 1 ]]; then
+                        cp ~/.local/share/zeesh/zshrc-stub ~/.zshrc
+                        _zshrc_updates_pending=1
+                fi
+        fi
+        
+        [[ _zshrc_updates_pending -eq 1 ]] && ticker_message "zeesh: stub updates pending"
+    fi
+}
+
+zeesh_update
