45e90a1baa266f4674947d66850e2fa61f347021
Nicole O'Connor <nicole@otl-hga.net>
2023-06-30 16:23:45 -0700

    zshrc.preload actually, so zeesh can see conda if it exists

--- modules/conda.zsh
+++ modules/conda.zsh
@@ -1,6 +1,6 @@
 # Note: location of conda can be unpredictable; user is expected to
 #       run `conda init zsh` as normal then move resulting .zshrc
-#       modifications to .zshrc.local
+#       modifications to .zshrc.preload
 
 # recommended settings for .condarc:
 #     auto_assign_base: false
